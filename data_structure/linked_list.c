5765a3a9be66740e13fd0bc351e878595ed21b9e
Matheus Morillo <matheus.morillo2000@gmail.com>
2021-03-07 00:34:53 -0300

    fix bug in linked list

--- data_structure/linked_list.c
+++ data_structure/linked_list.c
@@ -11,41 +11,40 @@ struct NodeInt {
   struct NodeInt *next;
 };
 
+struct LinkedList* get_empty_list();
 void push_int(struct LinkedList *linked_list, int value);
 void print_list(struct LinkedList *linked_list);
 
 int main() {
-  struct LinkedList *linked_list = (struct LinkedList*) malloc(sizeof(struct LinkedList));
+  struct LinkedList *linked_list = get_empty_list();
+
   char keep_going = '\0';
   int value = 0;
 
-  // do {
-  //   printf("Type a value to push: \n");
-  //   scanf("%i", &value);
+  do {
+    printf("Type a value to push: \n");
+    scanf("%i", &value);
 
-  //   push_int(linked_list, value);
+    push_int(linked_list, value);
 
-  //   printf("Do you wish to continue? Y/N\n");
-  //   scanf("%s", &keep_going);
-
-  // } while(keep_going == 'y' | keep_going == 'Y');
-
-  push_int(linked_list, 0);
-  printf("first insertion\n");
-  print_list(linked_list);
-  printf("\n");
-
-  push_int(linked_list, 1);
-  printf("second insertion\n");
-  print_list(linked_list);
-  printf("\n");
-
-  push_int(linked_list, 2);
-  printf("third insertion\n");
-  print_list(linked_list);
-  printf("\n");
+    printf("Do you wish to continue? Y/N\n");
+    scanf(" %c", &keep_going);
   
+
+  } while(keep_going == 'y' || keep_going == 'Y');
+
+  print_list(linked_list);
+  
+  system("PAUSE");
   return 0;
+}
+
+struct LinkedList* get_empty_list() {
+  struct LinkedList *linked_list = (struct LinkedList*) malloc(sizeof(struct LinkedList));
+  linked_list->first = NULL;
+  linked_list->last = NULL;
+
+  return linked_list;
 }
 
 void push_int(struct LinkedList *linked_list, int value) {
@@ -68,6 +67,7 @@ void push_int(struct LinkedList *linked_list, int value) {
 
 void print_list(struct LinkedList *linked_list) {
   struct NodeInt *curr_node = linked_list->first;
+
   for(int i = 0; curr_node != NULL; i++) {
     printf("Value of index %i: %i\n", i, curr_node->data);
     curr_node = curr_node->next;
