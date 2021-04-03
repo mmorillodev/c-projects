74729d2f495f4a49c255006fa570170c9097c838
Matheus Morillo <matheus.morillo2000@gmail.com>
2021-04-03 17:54:06 -0300

    some refactor

--- bit-to-byte.cpp
+++ bit-to-byte.cpp
@@ -6,12 +6,13 @@ typedef unsigned char uchar;
 
 int main() {
   uchar bits[] = {1, 1, 1, 1, 1, 1, 1, 1}; 
+  int arrSize = sizeof(bits)/sizeof(uchar);
 
   int output = 0;
 
   int i = 0;
-  int j = sizeof(bits)/sizeof(uchar) - 1;
-  while (i < 8) {
+  int j = arrSize - 1;
+  while (i < arrSize) {
     output |= bits[i] << j;
     i++;
     j--;
